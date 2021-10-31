9c5090b08165735d361632bf224304fcf576270f
sara isabel santacruz <saraisabel.santacruz.contractor@gdevtools.com>
2021-10-31 10:15:49 -0500

    se hace primera prueba con samplres

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var notaMidiMax = 95;
 
 function sketch(s) {
   let table;
-  let outputArte, outputCiencia, notas;
+  let outputArte, outputCiencia, outputTecnologia, notas;
   let newObject = {};
   let cont = 0;
 
@@ -21,6 +21,7 @@ function sketch(s) {
     let tableArray = table.getArray();
     const valoresArte = tableArray[0]; valoresArte.pop(); valoresArte.shift();
     const valoresCiencia = tableArray[1]; valoresCiencia.pop(); valoresCiencia.shift();
+    const valoresTecnologia = tableArray[2]; valoresTecnologia.pop(); valoresTecnologia.shift();
     const duracionReal = tableArray[26].map(item => parseInt(item)); duracionReal.pop(); duracionReal.shift();
     const maxNumberInterval = Math.max(...duracionReal);
     const minNumberInterval = Math.min(...duracionReal);
@@ -33,6 +34,9 @@ function sketch(s) {
       }, 
       ciencia: {
         notas: valoresCiencia
+      }, 
+      tecnologia: {
+        notas: valoresTecnologia
       }
     }
 
@@ -49,38 +53,31 @@ function sketch(s) {
       notas = notasArteNumbers.map(item => parseInt(s.map(item, minNumber, maxNumber, notaMidiMin, notaMidiMax)));
       newObject = {...newObject, [clase]: notas }
     })
-    let tiempoAnterior = s.millis();
-    let intervaloIndividual = 0;
-    let contador = 0;
-    while(s.millis() - tiempoAnterior >= intervaloIndividual) {
-      intervaloIndividual = intervalo[contador];
-      console.log(intervaloIndividual);
-      sendMidiNote();
-      contador++;
-      tiempoAnterior = s.millis();
-      if(contador >= intervalo.length - 1){
-        contador = 0;
-      }
-    }
+    setInterval(sendMidiNote, 1000);
   }
 
   function sendMidiNote() {
     const notasArte = newObject['arte'];
     const notasCiencia = newObject['ciencia'];
+    const notasTecnologia = newObject['tecnologia'];
     if(cont >= notas.length - 1) cont = 0;
     if(cont !== 0) {
       outputArte.stopNote(notasArte[cont], 1);
-      outputCiencia.stopNote(notasCiencia[cont], 2);
+      outputCiencia.stopNote(notasCiencia[cont], 1);
+      outputTecnologia.stopNote(notasTecnologia[cont], 1);
     }
     cont++;
+    console.log(notasArte[cont], notasCiencia[cont], notasTecnologia[cont]);
     outputArte.playNote(notasArte[cont], 1);
-    outputCiencia.playNote(notasCiencia[cont], 2);
+    outputCiencia.playNote(notasCiencia[cont], 1);
+    outputTecnologia.playNote(notasTecnologia[cont], 1);
   }
 
   WebMidi.enable(function(err) {
     if(err) { console.log("WebMidi could not be enabled.", err) }
     outputArte = WebMidi.getOutputByName("IAC Driver Bus 1"); //Sara
     outputCiencia = WebMidi.getOutputByName("IAC Driver Bus 2"); //Sara
+    outputTecnologia = WebMidi.getOutputByName("IAC Driver Bus 3"); //Sara
     //outputArte = WebMidi.getOutputByName("Driver IAC Bus 1"); //Rosario
   });
 
