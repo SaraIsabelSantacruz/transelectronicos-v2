d1d26dcc80e4dacd5ad439047400d6f712099e3e
sara isabel santacruz <saraisabel.santacruz.contractor@gdevtools.com>
2021-11-19 21:46:10 -0500

    index.html

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'docs'),
-    publicPath: "/webpack-demo/"
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/"
   },
   module: {
     rules: [
