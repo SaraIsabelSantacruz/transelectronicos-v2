2d1e145377fe672d55f1876869ac9535f3be9864
SaraIsabelSantacruz <sara.santacruz@wolox.co>
2022-08-18 23:17:17 -0300

    se agrega el tiempo y el valor de noteoff

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ let partitura;
 
 function sketch(s) {
   let table1, table2;
-  let outputArte, outputCiencia, outputTecnologia, notas;
+  let outputArte, outputCiencia, outputTecnologia, notas, tiempos;
   let newObject = {};
   let tableArray = [];
   let cont = 0;
@@ -76,57 +76,155 @@ function sketch(s) {
     //outputArte = WebMidi.getOutputByName("Driver IAC Bus 1"); //Rosario
   };
 
+  const functionToCreateNotes = (start, stop, step) => {
+    return Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step))
+  };
+
   function setValuesMidi(partitura) {
     const clases = Object.keys(partitura);
     clases.map(clase => {
       const notasNumbers = partitura[clase].notas?.map(item => item ? parseInt(item) : 0);
-      const getValue = (value) => {
-        for (let index = 0; index < notasNumbers.length; index++) {
-          if(value === notasNumbersOrder[index]) return escala[index];
+      const notasNumbersOrder = [ ...new Set(notasNumbers)].sort((a, b) => a-b);
+      if(clase === 'arte') { 
+        partitura['arte'].tiempo = functionToCreateNotes(0, 4, 2);
+        partitura['arte'].tiempo.reverse();
+        partitura['arte'].tiempo[partitura['arte'].tiempo.length-1] = 1;
+        partitura['arte'].tiempo.splice(0,0,0);
+        partitura['arte'].notasMidi = functionToCreateNotes(12, 48, 12);
+      }
+      if(clase === 'ciencia') {
+        partitura['ciencia'].tiempo = functionToCreateNotes(0, 4, 2);
+        partitura['ciencia'].tiempo.reverse();
+        partitura['ciencia'].tiempo[partitura['ciencia'].tiempo.length-1] = 1;
+        partitura['ciencia'].tiempo.splice(0,0,0);
+        partitura['ciencia'].notasMidi = functionToCreateNotes(19, 55, 12);
+      }
+      if(clase === 'ciencia') {
+        partitura['tecnologia'].tiempo = functionToCreateNotes(0, 4, 2);
+        partitura['tecnologia'].tiempo.reverse();
+        partitura['tecnologia'].tiempo[partitura['tecnologia'].tiempo.length-1] = 1;
+        partitura['tecnologia'].tiempo.splice(0,0,0);
+        partitura['tecnologia'].notasMidi = functionToCreateNotes(16, 52, 12);
+      }
+      const escala = partitura[clase].notasMidi;
+      const formasDeNota = partitura[clase].tiempo;
+      const getValueNotes = (value) => {
+        for (let i=0; i<notasNumbersOrder.length; i++) {
+          if (value === 0) return 0;
+          if(value === notasNumbersOrder[i]) {
+            let indexEscala = i;
+            if(i >= escala.length) {
+              const restaIndexEscala = escala.length - (notasNumbersOrder.length-1 - i);
+              indexEscala = restaIndexEscala-1;
+            }
+            return escala[indexEscala];
+          };
         }
       }
-      const notasNumbersOrder = [ ...new Set(notasNumbers)].sort((a, b) => a-b);
-      const escala = [0, 41, 43, 44, 46, 48, 50, 51, 53, 55, 56, 58, 60, 62, 63, 65, 67, 68, 70];
-      notas = notasNumbers.map(num => getValue(num));
-      newObject = {...newObject, [clase]: notas }
-      console.log(notas);
+      const getValueTimes = (value) => {
+        for (let i=0; i<notasNumbersOrder.length; i++) {
+          if (value === 0) return 0;
+          if(value === notasNumbersOrder[i]) {
+            let indexFormasNotas = i;
+            if(i >= formasDeNota.length) {
+              const restaIndexFormasNotas = formasDeNota.length - (notasNumbersOrder.length-1 - i);
+              indexFormasNotas = restaIndexFormasNotas-1;
+            }
+            return formasDeNota[indexFormasNotas];
+          };
+        }
+      }
+      notas = notasNumbers.map(num => getValueNotes(num));
+      tiempos = notasNumbers.map(num => getValueTimes(num));
+      newObject = {...newObject, [clase]: { notas, tiempos }};
     })
   }
 
-  function sendMidiNote() {
-    const notasArte = newObject['arte'];
-    const notasCiencia = newObject['ciencia'];
-    const notasTecnologia = newObject['tecnologia'];
+  function envioNotasArte() {
+    const notasArte = newObject['arte'].notas;
+    const tiempoArte = newObject['arte'].tiempos;
     if(cont >= notas.length - 1) cont = 0;
     if(cont !== 0) {
       outputArte.channels[1].stopNote(notasArte[cont]);
-      outputCiencia.channels[2].stopNote(notasCiencia[cont]);
-      outputTecnologia.channels[3].stopNote(notasTecnologia[cont]);
+    }
+    if(notasArte[cont] !== 0) {
+      outputArte.channels[1].playNote(notasArte[cont], { duration: tiempoArte[cont]*1000 });
+      console.log(cont, notasArte[cont], 'arte');
     }
     cont++;
-    outputArte.channels[1].playNote(notasArte[cont]);
-    outputCiencia.channels[2].playNote(notasCiencia[cont]);
-    outputTecnologia.channels[3].playNote(notasTecnologia[cont]);
-    console.log(notasArte[cont]);
   }
 
+  function envioNotasCiencia() {
+    const notasCiencia = newObject['ciencia'].notas;
+    const tiempoCiencia = newObject['ciencia'].tiempos;
+    if(cont >= notas.length - 1) cont = 0;
+    if(cont !== 0) {
+      outputCiencia.channels[2].stopNote(notasCiencia[cont]);
+    }
+    if(notasCiencia[cont] !== 0) {
+      outputCiencia.channels[2].playNote(notasCiencia[cont], { duration: tiempoCiencia[cont]*1000 });
+      console.log(cont, notasCiencia[cont], 'ciencia');
+    }
+    cont++;
+  }
+
+  function envioNotasTec() {
+    const notasTecnologia = newObject['tecnologia'].notas;
+    const tiempoTecnologia = newObject['tecnologia'].tiempos;
+    if(cont >= notas.length - 1) cont = 0;
+    if(cont !== 0) {
+      outputTecnologia.channels[3].stopNote(notasTecnologia[cont]);
+    }
+    if(notasTecnologia[cont] !== 0 ) {
+      outputTecnologia.channels[3].playNote(notasTecnologia[cont], { duration: tiempoTecnologia[cont]*1000 });
+      console.log(cont, notasTecnologia[cont], 'Tecnologia');
+    }
+    cont++;
+  }
+
+  let delayArte = 0; let delayCiencia=0; let delayTec = 0;
+  let timerArte, timerCiencia, timerTec;
+  let timeArte, timeCiencia, timeTec = 0;
+  
   function playNotes() {
-    interval = setInterval(sendMidiNote, 1000);
+    timerArte = setTimeout(function request() {
+      if(delayArte <= 24) {
+        timeArte = newObject['arte'].tiempos[delayArte]*1000;
+        envioNotasArte();
+        delayArte++;
+        timerArte = setTimeout(request, timeArte);
+        if(delayArte > 24) delayArte = 0;
+      }
+    }, timeArte);
+
+    timerCiencia = setTimeout(function request() {
+      if(delayCiencia <= 24) {
+        timeCiencia = newObject['ciencia'].tiempos[delayCiencia]*1000;
+        envioNotasCiencia();
+        delayCiencia++;
+        timerCiencia = setTimeout(request, timeCiencia);
+        if(delayCiencia > 24) delayCiencia = 0;
+      }
+    }, timeCiencia);
+
+    timerTec = setTimeout(function request() {
+      if(delayTec <= 24) {
+        timeTec = newObject['tecnologia'].tiempos[delayTec]*1000;
+        envioNotasTec();
+        delayTec++;
+        timerTec = setTimeout(request, timeTec);
+        if(delayTec > 24) delayTec = 0;
+      }
+    }, timeTec);
   }
 
   function stopNotes() {
-    clearInterval(interval);
+    clearTimeout(timerArte);
+    clearTimeout(timerCiencia);
+    clearTimeout(timerTec);
     cont++;
   }
 
   s.draw = () => {}
 }
-
-//frecuencia
-//.h con las categorías y su orden en el arreglo
-//npm install
-//npm run server
-//corre en local host 8080
-//agregar un h1 o p que indique corriendo con éxito
-
 new p5(sketch);
